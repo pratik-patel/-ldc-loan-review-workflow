9c3b714cfa52a8eb5db5fd573c5028b3b85d40ae
Pratik Patel <pratik_patel@live.in>
2025-12-22 12:50:07 -0500

    removed notifications, sqs, ses, timer logic from reclass approved and repurchase

--- terraform/modules/ses/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "sender_email" {
-  description = "SES sender email address"
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment name"
-  type        = string
-}
